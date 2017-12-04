1fc956729f4f25d69e8ee6e07b3b81252ab5bfa9
Test <musty.dalga@gmail.com>
2017-12-04 14:12:31 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SayiyiMetinOlarakYazdirma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSonuc = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSonuc
+            // 
+            this.btnSonuc.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSonuc.ForeColor = System.Drawing.Color.Lime;
+            this.btnSonuc.Location = new System.Drawing.Point(190, 10);
+            this.btnSonuc.Name = "btnSonuc";
+            this.btnSonuc.Size = new System.Drawing.Size(75, 27);
+            this.btnSonuc.TabIndex = 0;
+            this.btnSonuc.Text = "Sonuç";
+            this.btnSonuc.UseVisualStyleBackColor = false;
+            this.btnSonuc.Click += new System.EventHandler(this.btnSonuc_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bir sayı gir";
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.AutoSize = true;
+            this.lblsonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsonuc.ForeColor = System.Drawing.Color.Lime;
+            this.lblsonuc.Location = new System.Drawing.Point(81, 41);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(52, 17);
+            this.lblsonuc.TabIndex = 3;
+            this.lblsonuc.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.ForeColor = System.Drawing.Color.Lime;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 104);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 173);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Yazılışı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(361, 305);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSonuc);
+            this.Name = "Form1";
+            this.Text = "Sayıyı Metne Çevirme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSonuc;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiyiMetinOlarakYazdirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSonuc_Click(object sender, EventArgs e)
+        {
+       
+            try
+            {
+                int[] sayibasamaklari = new int[4];
+                int i = 0;
+                string metin = "";
+                string[] bsmk = { "birler", "onlar", "yüzler", "binler" };
+                string[,] basamaklarstring = { { "", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" }, { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" }, { "", "yüz", "iki yüz", "üç yüz", "dört yüz", "beş yüz", "altı yüz", "yedi yüz", "sekiz yüz", "dokuz yüz" }, { "", "bin", "iki bin", "üç bin", "dört bin", "beş bin", "altı bin", "yedi bin", "sekiz bin", "dokuz bin" } };
+                int[] basamaklarint = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                int sayi = Convert.ToInt32(textBox1.Text);
+                while (sayi > 0)
+                {
+                    sayibasamaklari[i] = sayi % 10;
+                    listBox1.Items.Add(bsmk[i]+" basamağı:"+sayibasamaklari[i]);
+                    sayi = sayi / 10;
+                    i++;
+                }
+                for (i = sayibasamaklari.Length - 1; i >= 0; i--)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (sayibasamaklari[i] == basamaklarint[j])
+                        {
+                            metin += basamaklarstring[i, j] + " ";
+                        }
+                    }
+                }
+                lblsonuc.Text = metin;
+            }
+            catch (Exception)
+            {
+                listBox1.Items.Clear();
+                string metin = textBox1.Text;
+                bool durum = true;
+                for (int i = 0; i < metin.Length; i++)
+                {
+                    if (Char.IsLetter(metin[i]))
+                    {
+                        MessageBox.Show("Lütfen sayı girişi yapınız");
+                        durum = false;
+                        break;
+                    }
+                }
+
+                if (durum==true && metin.Length> 4)
+                {
+                    MessageBox.Show("Lütfen en az 4 basamaklı sayıları girerek işlem yapınız");
+                }
+            }
+        }
+    }
+}
